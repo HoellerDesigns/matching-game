6a338433592ecd0ebd77b7ba1da89ddcb5708500
Nathan Hoeller <contact@hoellerdesigns.com>
2016-02-09 09:44:27 -0500

    Added flexibility to script to allow any amount of cards. Also fixed responsive styles, and added a click counter to keep score

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/*
+ *
+ * main.js for Matching Game
+ *
+ * Developed by Nathan Hoeller
+ * www.hoellerdesigns.com
+ *
+ */
+
 jQuery(function($) {
 
     // Fastclick script used to load touch functions faster
@@ -11,52 +20,46 @@ jQuery(function($) {
         "B.jpg",
         "C.jpg",
         "D.jpg",
-        "E.jpg"
+        "E.jpg",
+        "F.jpg"
     ];
     // Set Path to Card Images Here:
     var imgSrc = "img/";
 
+    // Set the initial click value to 0
+    var clickCount = 0;
 
     // Loop through each card in the array and generate HTML for the matching pair
-    var cardboxOutput = "";
+    var deckOutput = "";
     var cardCount = cardArray.length;
     var count = 1;
     for (i = 0; i < cardCount; i++) {
         var output = '<div class="flip-container">'
-                +'<div id="card'+count+'" class="card">'
+                +'<div id="card'+count+'" data-card="'+count+'" class="card">'
                     +'<div class="front"></div>'
                     +'<div class="back"><img src="'+imgSrc+cardArray[i]+'"/></div>'
                 +'</div>'
             +'</div>';
         count++;
         output += '<div class="flip-container">'
-                +'<div id="card'+count+'" class="card">'
+                +'<div id="card'+count+'" data-card="'+count+'" class="card">'
                     +'<div class="front"></div>'
                     +'<div class="back"><img src="'+imgSrc+cardArray[i]+'"/></div>'
                 +'</div>'
             +'</div>';
         count++;
-        cardboxOutput += output;
+        deckOutput += output;
     }
-    $("#cardbox").html(cardboxOutput);
+    $("#deck").html(deckOutput);
 
-    var card1Checked = null;
-    var card2Checked = null;
-    var card3Checked = null;
-    var card4Checked = null;
-    var card5Checked = null;
-    var card6Checked = null;
-    var card7Checked = null;
-    var card8Checked = null;
-    var card9Checked = null;
-    var card10Checked = null;
-
-
-    // Randomize the order of the cards
-    randomize();
+    randomize(); // Randomize the order of the cards
 
     var numFlips = 0;
     $(".card").click(function() {
+        // Update click count
+        clickCount++;
+        $("#clickCount span").html(clickCount);
+
         if (!($(this).hasClass("matched"))) {
             numFlips = $('.flip').length;
             if (numFlips < 1) {
@@ -70,99 +73,13 @@ jQuery(function($) {
 
     $("#resetCards").click(function() {
         $(".card").removeClass("flip matched");
+        $("#clickCount span").html("0");
         setTimeout(function(){
             randomize();
         }, 500);
     });
-});
+}); // Document Ready
 
-
-function isOdd(num) {
-    return num % 2;
-}
-
-function checkMatch() {
-    var checkCards = $(".flip").toArray();
-    var c1 = checkCards[0].attr("id");
-    var c2 = checkCards[1];
-    //var c1ID = c1.attr("id");
-    //var c2ID = c2.attr("id");
-
-    // return id, then remove "card" string
-    console.log(c1);
-    console.log(c2);
-
-    if (isOdd(c1)) {
-        // It's match would need to be plus one.
-        if (Number(c1) + 1 === c2) {
-            console.log("MATCH!");
-        } else {
-            setTimeout(function(){
-                $(".card").removeClass("flip");
-            }, 700);
-        }
-    } else {
-        // It's match would need to be minus one.
-        if (Number(c1) - 1 === c2) {
-            console.log("MATCH!");
-        } else {
-            setTimeout(function(){
-                $(".card").removeClass("flip");
-            }, 700);
-        }
-    }
-
-    finished();
-
-    /*$(".flip").each(function(i) {
-        var cardID = $(this).attr('id');
-        var cardNum = cardID.replace('card', '');
-        if (isOdd(cardNum)) {
-            console.log(cardNum+" is Odd");
-        } else {
-            console.log(cardNum);
-        }
-    });*/
-    /*card1Checked = $("#card1.flip").length;
-    card2Checked = $("#card2.flip").length;
-    card3Checked = $("#card3.flip").length;
-    card4Checked = $("#card4.flip").length;
-    card5Checked = $("#card5.flip").length;
-    card6Checked = $("#card6.flip").length;
-    card7Checked = $("#card7.flip").length;
-    card8Checked = $("#card8.flip").length;
-    card9Checked = $("#card9.flip").length;
-    card10Checked = $("#card10.flip").length;
-    if ((card1Checked === 1) && (card2Checked === 1)) {
-        $("#card1").addClass("matched").removeClass("flip");
-        $("#card2").addClass("matched").removeClass("flip");
-    } else if ((card3Checked === 1) && (card4Checked === 1)) {
-        $("#card3").addClass("matched").removeClass("flip");
-        $("#card4").addClass("matched").removeClass("flip");
-    } else if ((card5Checked === 1) && (card6Checked === 1)) {
-        $("#card5").addClass("matched").removeClass("flip");
-        $("#card6").addClass("matched").removeClass("flip");
-    } else if ((card7Checked === 1) && (card8Checked === 1)) {
-        $("#card7").addClass("matched").removeClass("flip");
-        $("#card8").addClass("matched").removeClass("flip");
-    } else if ((card9Checked === 1) && (card10Checked === 1)) {
-        $("#card9").addClass("matched").removeClass("flip");
-        $("#card10").addClass("matched").removeClass("flip");
-    } else {
-        setTimeout(function(){
-            $(".card").removeClass("flip");
-        }, 700);
-    }
-    finished();*/
-};
-
-function finished() {
-    if ($(".matched").length === 10) {
-        setTimeout(function(){
-            alert("Contratulations! You win!");
-        }, 700);
-    }
-};
 
 function randomize(){
     //get list of divs
@@ -172,6 +89,45 @@ function randomize(){
     while(deck.length > 0) {
         var idx = Math.floor(Math.random() * deck.length);
         var element = deck.splice(idx, 1);
-        $('#cardbox').append(element[0]);
+        $('#deck').append(element[0]);
     }
-};
+}
+
+
+function isOdd(num) {
+    return num % 2;
+}
+
+
+function checkMatch() {
+    var count = 0;
+    var c1 = "";
+    $(".flip").each(function(i) {
+        var cardNum = $(this).data("card");
+        if (count === 0) {
+            c1 = cardNum;
+            count++;
+        } else {
+            var c1Match = (isOdd(c1)) ? c1 + 1 : c1 - 1;
+            if (c1Match === cardNum) {
+                $("#card"+c1).addClass("matched").removeClass("flip");
+                $("#card"+cardNum).addClass("matched").removeClass("flip");
+            } else {
+                setTimeout(function(){
+                    $(".card").removeClass("flip");
+                }, 700);
+            }
+        }
+    });
+    finished(); // Check if game is finished
+}
+
+
+function finished() {
+    var cardCount = $(".card").length;
+    if ($(".matched").length === cardCount) {
+        setTimeout(function(){
+            alert("Contratulations! You win!");
+        }, 700);
+    }
+}
